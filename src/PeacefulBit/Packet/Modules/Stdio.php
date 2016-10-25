4f7e0bf94bb0d319a5fdba83a024d2fcdc270cbd
Roman Gemini <roman@homefs.biz>
2016-10-25 09:31:55 +0300

    fix readline

--- src/PeacefulBit/Packet/Modules/Stdio.php
+++ src/PeacefulBit/Packet/Modules/Stdio.php
@@ -15,7 +15,8 @@ function export()
                 throw new RuntimeException('Function expects one argument, but none given');
             }
             echo $visitor->valueOf($arguments[0]);
-            return new StringNode(fgetc(STDIN));
+            echo ' ';
+            return new StringNode(fgets(STDIN));
         }),
         'print' => new NativeNode('print', function (NodeCalculatorVisitor $visitor, array $arguments) {
             array_walk($arguments, function ($argument) use ($visitor) {
